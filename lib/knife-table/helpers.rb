e6ffcd1388ec99ef26d241f5876d70f0de09c953
Chris Roberts <chrisroberts.code@gmail.com>
2012-07-15 13:56:40 -0700

    Extract change discovery out to helper

--- lib/knife-table/helpers.rb
+++ lib/knife-table/helpers.rb
@@ -7,5 +7,15 @@ module KnifeTable
     def cookbook_path
       Chef::Config[:cookbook_path].first
     end
+
+    def discover_changed(type, first_commit, last_commit)
+      changed = []
+      git.diff(first_commit, last_commit).stats[:files].keys.each do |path|
+        if(path.start_with?(type.to_s))
+          changed << path.sub(/^#{type.to_s}\/?/, '')
+        end
+      end
+      changed.uniq
+    end
   end
 end
